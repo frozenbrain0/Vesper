edea4448ef789bfe847024031aed1e0a7b5c485c
xenon <olivander@mail.de>
2026-01-20 20:38:00 +0100

    Working

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 int main() {
     // Start the server
     http::HttpServer server("localhost", 8080);
+
+    server.setHandler([](http::HttpConnection& conn) {
+        conn.sendPlainText(http::HttpResponse::StatusCodes::OK, "Hello World\n");
+    });
+
     server.run();
 
     return 0;
